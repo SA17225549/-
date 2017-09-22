307c926ba2d9889589b09855710dfcd0cf49b345
18463103071 <18463103071@163.com>
2017-09-22 20:50:05 +0800

    menu

---
+++ lab2/menu.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+void hello(){
+printf("my name is sa17225549");
+}
+int main(){
+char cmd[256];
+while(1){
+scanf("%s",cmd);
+if(strcmp(cmd,"hello"==0)){
+hello();
+}
+else{
+printf("error command");
+}
+}
+}
