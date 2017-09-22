4725c2bdce86a7b2a9c8d28a6fe2f3c5570fd904
SA17225549 <31922498+SA17225549@users.noreply.github.com>
2017-09-22 21:02:57 +0800

    Update menu.c

--- lab2/menu.c
+++ lab2/menu.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+void menu(){
+}
 void hello(){
 printf("my name is sa17225549");
 }
@@ -8,7 +10,7 @@ int main(){
   while(1)
   {
     scanf("%s",cmd);
-    if(strcmp(cmd,"hello")==0))
+    if(strcmp(cmd,"hello")==0)
     {
       hello();
     }
